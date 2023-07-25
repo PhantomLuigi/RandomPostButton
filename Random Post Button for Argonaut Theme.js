acf08289f2a0ff582195feb6df8607f3f59cb1fe
PhantomLuigi <PhantomLuigi08@gmail.com>
2023-07-25 12:01:57 +1000

    Update Random Post Button for Argonaut Theme.js

--- Random Post Button for Argonaut Theme.js
+++ Random Post Button for Argonaut Theme.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Random Post Button for Argonaut Theme
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  Brings you to a random post
 // @author       PhantomLuigi
 // @match        https://tbgforums.com/forums/index.php
@@ -32,7 +32,7 @@ function init_css(){
     return `
     <button id="random">Random Post</button>
     <style>
-        #random{position:absolute;top:70px;left:65%;height:70px;width:130px;}
+        #random{position:absolute;top:66px;left:65%;height:70px;width:130px;}
         #random{background:#33b5e5;color:white;padding-top:0px;text-align:center;}
         #random{z-index:0;}
     </style>
